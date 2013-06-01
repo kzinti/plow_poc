253a1143a65d7cd54809fad332dc618e05a7867b
kzinti <kzintyempire@gmail.com>
2013-06-01 12:16:30 -0400

    inittial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlowPoc::Application.config.secret_token = '7e7c2d176e9acbfb3a84ce8c7ac2ebf7d46d66eb97f27b97c4d1b93727b44753304f37fded77fd68622c9b9ad4e52504cd87d16b81701c9d4e70bddc0d30a50d'
